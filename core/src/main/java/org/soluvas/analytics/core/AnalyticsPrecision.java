92c919c87898e1acf792ed5c58b80ec0abf7355b
Hendy Irawan <ceefour666@gmail.com>
2014-05-30 05:06:16 -0430

    OPEN - issue GRD-91: Date-range based reporting (today, yesterday, last week, custom) https://quikdo.atlassian.net/browse/GRD-91

---
+++ core/src/main/java/org/soluvas/analytics/core/AnalyticsPrecision.java
@@ -0,0 +1,13 @@
+package org.soluvas.analytics.core;
+
+/**
+ * Precision of an analytics data point.
+ * @author ceefour
+ */
+public enum AnalyticsPrecision {
+	QUARTER_HOURLY,
+	HOURLY,
+	DAILY,
+	WEEKLY,
+	MONTHLY
+}
